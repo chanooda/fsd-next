5688214980eec1c15d10d25ddcf4c1b9dfb554c7
chanooda <hanrhfqkq@gmail.com>
2024-12-09 20:12:30 +0900

    design: FeedPage 스타일 적용 완료

--- src/pages/feed/ui/ArticlePreview.tsx
+++ src/pages/feed/ui/ArticlePreview.tsx
@@ -5,6 +5,7 @@ import {
   AvatarFallback,
   AvatarImage,
 } from "@/shared/ui/shadcn/components/ui/avatar";
+import { Heart } from "lucide-react";
 import Link from "next/link";
 
 interface ArticlePreviewProps {
@@ -13,16 +14,16 @@ interface ArticlePreviewProps {
 
 export const ArticlePreview = ({ article }: ArticlePreviewProps) => {
   return (
-    <div>
-      <div>
+    <div className="flex flex-col gap-2 border-b py-4">
+      <div className="flex items-center gap-2">
         <Link href={`/profile/${article.expand.author.id}`}>
           {article.expand.author.avatar ? (
-            <Avatar>
+            <Avatar className="h-6 w-6">
               <AvatarImage
                 src={getFileUrl(
                   article.expand.author.collectionName,
                   article.expand.author.id,
-                  article.expand.author.avatar
+                  article.expand.author.avatar,
                 )}
               />
               <AvatarFallback>
@@ -32,28 +33,32 @@ export const ArticlePreview = ({ article }: ArticlePreviewProps) => {
           ) : null}
         </Link>
         <div>
-          <Link href={`/profile/${article.expand.author.user}`}>
+          <Link href={`/profile/${article.expand.author.id}`}>
             {article.expand.author.username}
           </Link>
-          <span suppressHydrationWarning>
-            {new Date(article.created).toLocaleDateString(undefined, {
-              dateStyle: "long",
-            })}
-          </span>
         </div>
-        <button>
-          <i></i> {article.favoritesCount}
-        </button>
       </div>
-      <Link href={`/article/${article.slug}`}>
-        <h1>{article.title}</h1>
-        <p>{article.description}</p>
-        <span>Read more...</span>
-        <ul>
-          {article?.tagList?.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+      <Link href={`/article/${article.id}`}>
+        <div className="flex flex-col">
+          <h1 className="text-2xl">{article.title}</h1>
+          <p className="mt-1 text-gray-600">{article.description}</p>
+          <div className="mt-3 flex items-center gap-2 text-xs text-gray-600">
+            <span suppressHydrationWarning>
+              {new Date(article.created).toLocaleDateString(undefined, {
+                dateStyle: "long",
+              })}
+            </span>
+            <button>
+              <div className="flex items-center gap-1">
+                <Heart size={18} className="fill-red-400 stroke-red-400" />
+                {article.favoritesCount || 0}
+              </div>
+            </button>
+            <ul className="flex">
+              {article?.tagList?.map((tag) => <li key={tag}>{tag}</li>)}
+            </ul>
+          </div>
+        </div>
       </Link>
     </div>
   );
--- src/pages/feed/ui/FeedPage.tsx
+++ src/pages/feed/ui/FeedPage.tsx
@@ -6,9 +6,9 @@ export const FeedPage = async () => {
 
   return (
     <div className="flex flex-col">
-      <div className="flex justify-center items-center py-12 h-40 bg-[#242424] ">
+      <div className="flex items-center justify-center bg-[#242424] py-12">
         <div>
-          <div className="text-white text-center flex flex-col  gap-2">
+          <div className="flex flex-col gap-1 text-center text-white">
             <h1 className="text-2xl">conduit</h1>
             <p>A place to share your knowledge.</p>
           </div>
@@ -16,7 +16,7 @@ export const FeedPage = async () => {
       </div>
       <div>
         <div>
-          <div>
+          <div className="mx-auto max-w-[960] px-4 py-8">
             {articles?.items.map((article) => (
               <ArticlePreview key={article.slug} article={article} />
             ))}
