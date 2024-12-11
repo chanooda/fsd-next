0dc179d45d8ff6c3d8142c2949c633f203d23e24
chanooda <hanrhfqkq@gmail.com>
2024-12-11 15:57:45 +0900

    design: Tabs 컴포넌트 추가

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -6,3 +6,9 @@ export {
 } from "./shadcn/components/ui/avatar";
 export { Badge } from "./shadcn/components/ui/badge";
 export { Button } from "./shadcn/components/ui/button";
+export {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "./shadcn/components/ui/tabs";
